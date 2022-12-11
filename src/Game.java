b3cc0ac6947bf43c931772acb21d3ea2e0d56063
Amine Naqi <amine.naqi@vub.be>
2022-12-11 16:10:42 +0100

    Added some player functions

--- src/Game.java
+++ src/Game.java
@@ -1,37 +1,53 @@
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Stack;
 
 public class Game {
 	
+	enum GameState 
+	{
+		gettingInfo,
+		runGame
+	}
+	
+	private GameState game;
+	
+	private boolean gameRunning;
+	
 	private int numberOfPlayers, drawLimit,
 				playLimit      , keeperLimit,
 				handLimit;
 	
-	private Card card; 
 	private ArrayList<Player> players;
-	private Stack<Card> pile;
-	private Deck deck;
-	private ArrayList<Card> cardsOnTable;
+	private Stack<Card>       pile;
+	private Deck              deck;
+	private ArrayList<Card>   cardsOnTable;
 	
-	public Game(int _numOfPlayers)
-	{
-		this.numberOfPlayers = _numOfPlayers;
-		this.drawLimit = 1;
-		this.playLimit = 1;
+	public Game(int _userInput) throws FileNotFoundException
+	{	
+		this.deck = new Deck();
+		this.players = new ArrayList<Player>(); 
+		this.numberOfPlayers = _userInput;	
+		
+		for(int i = 0 ; i < this.numberOfPlayers ; i++)
+		{
+			this.players.add(new Player("Player_" + (i + 1)));
+		}
+		this.drawLimit   = 1;
+		this.playLimit   = 1;
 		this.keeperLimit = 100;
-		this.handLimit = 100;
+		this.handLimit   = 100;
+		this.gameRunning = true;
 	}
 	
 	public Card drawCard()
 	{
-
-		return null;
+		return this.deck.getDeckStack().pop();
 	}
 	
-	public Card throwCard()
+	public void throwCard(Card _card)
 	{
-
-		return null;
+		this.pile.add(_card);
 	}
 	
 	public Card placeCard(Card _card)
@@ -39,6 +55,19 @@ public class Game {
 		
 		return null;
 	}
-
 	
+	public void run(int _userInput)
+	{
+		this.players.get(0).getCardsInHand().add(drawCard());
+		this.players.get(0).showHand();
+		//drawCard();
+	}
+
+	public int getNumberOfPlayers() {
+		return this.numberOfPlayers;
+	}
+
+	public boolean isGameRunning() {
+		return this.gameRunning;
+	}
 }
--- src/Player.java
+++ src/Player.java
@@ -3,17 +3,16 @@ import java.util.ArrayList;
 public class Player {
 	
 	private String playerName;
-	// We don't need numberofcards in hand we can just get the size of the cardsinhand
 	private int numberOfKeepersInHand;
 	
-	private  ArrayList<Keepers> keepersOnTable;
+	private  ArrayList<Keepers> keepersOnSideTable;
 	private ArrayList<Card> cardsInHand;
 	
 	public Player(String _playerName)
 	{
 		this.playerName = _playerName;
 		this.numberOfKeepersInHand = 0;
-		this.keepersOnTable = new ArrayList<Keepers>();
+		this.keepersOnSideTable = new ArrayList<Keepers>();
 		this.cardsInHand = new ArrayList<Card>();
 	}
 
@@ -30,10 +29,36 @@ public class Player {
 	}
 
 	public ArrayList<Keepers> getKeepersOnTable() {
-		return keepersOnTable;
+		return keepersOnSideTable;
 	}
 
 	public ArrayList<Card> getCardsInHand() {
 		return cardsInHand;
 	}
+	
+	public void showHand()
+	{
+		for(int i = 0 ; i < this.cardsInHand.size() ; i++)
+		{
+			if(this.cardsInHand.get(i).getClass() == Rules.class)
+			{
+				Rules rule = (Rules) cardsInHand.get(i);
+				System.out.println(rule.getRuleName());
+			}
+			else if(this.cardsInHand.get(i).getClass() == Keepers.class)
+			{
+				Keepers rule = (Keepers) cardsInHand.get(i);
+				System.out.println(rule.getKeeperName());
+			}
+			else if(this.cardsInHand.get(i).getClass() == Goals.class)
+			{
+				Goals rule = (Goals) cardsInHand.get(i);
+				System.out.println(rule.getGoalName());
+			}
+		}
+		
+		System.out.println(this.cardsInHand.size());
+	}
+	
+	
 }
